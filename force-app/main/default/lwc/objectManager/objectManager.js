d48083ff573602a8e158f0f33e58e518cf5b6567
mmilidoni <michelemilidoni@gmail.com>
2022-08-24 22:43:31 +0200

    Moved apex method to wired function

--- force-app/main/default/lwc/objectManager/objectManager.js
+++ force-app/main/default/lwc/objectManager/objectManager.js
@@ -1,60 +1,51 @@
-import { LightningElement } from 'lwc';
-import getObjectsList from '@salesforce/apex/ObjectManagerCntrl.getObjectsList';
+import { LightningElement, wire, track } from 'lwc';
 import findObject from '@salesforce/apex/ObjectManagerCntrl.findObject';
+import { refreshApex } from '@salesforce/apex';
 
 const columns = [
     { label: 'Label', fieldName: 'label' },
     { label: 'Plural Label', fieldName: 'pluralLabel' },
     { label: 'API Name', fieldName: 'apiName'},
     { label: 'Type', fieldName: 'type'}
-];
+]; 
 
 // search delay
 const DELAY = 350;
 
-
 export default class ObjectManager extends LightningElement {
 
-    data = [];
+    @track data = [];
     columns = columns;
-    queryTerm;
-    showSpinner;
+    queryTerm = '';
 
-    connectedCallback() {
-        this.showSpinner = true;
-        getObjectsList()
-            .then(result => {
-                // sorting by Object label
-                result.sort((a,b) => (a.label > b.label) ? 1 : ((b.label > a.label) ? -1 : 0));
-                this.data = result;
-                this.showSpinner = false;
-            })
-            .catch(error => {
-                alert(error);
-                this.showSpinner = false;
-            });
+    @wire(findObject, {
+        keyword: "$queryTerm"
+    }) wiredSObjects(result) {
+        this.data = null;
+        console.log("result", result);
+        if (result.data) {
+            this.data = [...result.data].sort((a,b) => (a.label > b.label) ? 1 : ((b.label > a.label) ? -1 : 0));
+        }
+        if (result.error) {
+            console.error("Error", error);
+        }
     }
+
+    get showSpinner() {
+        return !this.data && !this.error;
+      }
+      
 
     handleSearchInputChange(evt){
         this.queryTerm = evt.target.value;
-        // find objects with a delayed search
-        this.doDelayedSearch();
+        window.clearTimeout(this.delayTimeout);
+        this.delayTimeout = setTimeout(this.doSearch.bind(this), DELAY);
+
     }
 
-    doDelayedSearch() {
-       window.clearTimeout(this.delayTimeout);
-       this.delayTimeout = setTimeout(() => {
-           findObject({keyword: this.queryTerm})
-               .then(result => {
-                   result.sort((a,b) => (a.label > b.label) ? 1 : ((b.label > a.label) ? -1 : 0));
-                   this.data = result
-                   this.showSpinner = false;
-               })
-               .catch(error => {
-                   alert(JSON.stringify(error));
-                   this.showSpinner = false;
-               });
-       }, DELAY);
+    doSearch() {
+        this.data = null;
+        refreshApex(this.data);
     }
 
 }
